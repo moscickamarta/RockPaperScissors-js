88d2429141911d9bdcf63ddab0bcbd2efc31bffd
moscickamarta <249362937+moscickamarta@users.noreply.github.com>
2025-12-22 14:07:37 +0100

    created getComputerChoice() and stored it's value in a variable

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+function getComputerChoice(){
+  const random = Math.random();
+  if(random>=0.66)
+    return 'Rock';
+  else if(random>=0.33)
+    return 'Paper';
+  else return 'Scissors';
+}
+
+const compPick=getComputerChoice();
+console.log(compPick);
