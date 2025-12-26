58fd81a67360369c44c449b24ab03d469646694e
moscickamarta <249362937+moscickamarta@users.noreply.github.com>
2025-12-26 16:29:12 +0100

    created function playRound() that plays the game one time. Made choices case insensitive.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function getComputerChoice(){
   const random = Math.random();
   if(random>=0.66)
-    return 'Rock';
+    return 'rock';
   else if(random>=0.33)
-    return 'Paper';
-  else return 'Scissors';
+    return 'paper';
+  else return 'scissors';
 }
 
 const compPick=getComputerChoice();
@@ -16,3 +16,56 @@ function getHumanChoice(){
 
 const humanPick=getHumanChoice();
 console.log(humanPick);
+
+const humanScore = 0;
+const computerScore = 0;
+let result;
+
+function playRound(humanChoice, computerChoice){
+ if(humanChoice.toLowerCase()=='scissors'){
+  switch(computerChoice){
+    case 'scissors':
+      result='tie';
+      break;
+    case 'paper':
+      result='win';
+      break;
+    case 'rock':
+      result='loose';
+      break;
+  }
+ }
+ else if(humanChoice.toLowerCase()=='rock'){
+  switch(computerChoice){
+    case 'scissors':
+      result='win';
+      break;
+    case 'paper':
+      result='loose';
+      break;
+    case 'rock':
+      result='tie';
+      break;
+  }
+ }
+ else if (humanChoice.toLowerCase()=='paper'){
+  switch(computerChoice){
+    case 'scissors':
+      result='loose';
+      break;
+    case 'paper':
+      result='tie';
+      break;
+    case 'rock':
+      result='win';
+      break;
+  }
+ }
+ else{
+  console.log('error')
+ }
+
+ console.log(`You picked ${humanChoice}, computer picked ${compPick}. You ${result}`);
+}
+
+playRound(humanPick, compPick);
